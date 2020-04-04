0fec5cf669d70b439510b427179820f0e51495a3
Asim Amalani <asim.amalani@gmail.com>
2020-04-04 14:43:08 -0400

    FindNeedleInHaystack StrStr SubString problem

---
+++ LeetCodeProblems.Tests/FindNeedleInHaystack_StrStrShould.cs
@@ -0,0 +1,61 @@
+using Xunit;
+
+namespace LeetCode.Problems.UnitTests
+{
+    public class FindNeedleInHaystack_StrStrShould
+    {
+        private readonly FindNeedleInHaystack _findNeedleInHaystack;
+
+        public FindNeedleInHaystack_StrStrShould()
+        {
+            _findNeedleInHaystack = new FindNeedleInHaystack();
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(null, "")]
+        [InlineData("", null)]
+        [InlineData("", "")]
+        [InlineData("   ", "   ")]
+        [InlineData("haystack", "   ")]
+        [InlineData("    ", "stack")]
+        [InlineData("haystack", "haystack")]
+        public void StrStr_InputNullOrWhitespace_Return0(string haystack, string needle)
+        {
+            var result = _findNeedleInHaystack.StrStr(haystack, needle);
+            Assert.StrictEqual<int>(0, result);
+        }
+
+        [Theory]
+        [InlineData("haystack", "s")]
+        [InlineData("haystack", "st")]
+        [InlineData("haystack", "sta")]
+        [InlineData("haystack", "stac")]
+        [InlineData("haystack", "stack")]
+        public void StrStr_InputValid_ReturnIndex3(string haystack, string needle)
+        {
+            var result = _findNeedleInHaystack.StrStr(haystack, needle);
+            Assert.StrictEqual<int>(3, result);
+        }
+
+        [Theory]
+        [InlineData("haystack", "a")]
+        public void StrStr_InputValidMultipleMatches_ReturnIndex1(string haystack, string needle)
+        {
+            var result = _findNeedleInHaystack.StrStr(haystack, needle);
+            Assert.StrictEqual<int>(1, result);
+        }
+
+        [Theory]
+        [InlineData("haystack", "b")]
+        [InlineData("haystack", "stack1")]
+        [InlineData("haystack", "haystack1")]
+        [InlineData("haystack", "aaystack")]
+        [InlineData("haystack", "needle")]
+        public void StrStr_InputInvalidNoMatches_ReturnIndexMinus1(string haystack, string needle)
+        {
+            var result = _findNeedleInHaystack.StrStr(haystack, needle);
+            Assert.StrictEqual<int>(-1, result);
+        }
+    }
+}
---
+++ LeetCodeProblems/FindNeedleInHaystack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    public class FindNeedleInHaystack
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack == needle || String.IsNullOrWhiteSpace(haystack) || String.IsNullOrWhiteSpace(needle))
+            {
+                return 0;
+            }
+
+            var haystackLength = haystack.Length;
+            int needleLength = needle.Length;
+            var resultIndex = -1;
+            
+            if (needleLength > haystackLength)
+            {
+                return resultIndex;
+            }
+
+            if (needleLength == haystackLength && needle[0] != haystack[0])
+            {
+                return resultIndex;
+            }
+
+            var needleIndexCounter = 0;
+            
+            for (int i = 0; i < haystackLength; i++)
+            {
+                var haystackChar = haystack[i];
+
+                if (needleIndexCounter < needleLength)
+                {
+                    if (haystackChar == needle[needleIndexCounter])
+                    {
+                        if (needleIndexCounter == 0)
+                        {
+                            if (haystackLength - i < needleLength)
+                            {
+                                return -1;
+                            }
+                            resultIndex = i;
+                        }
+
+                        needleIndexCounter++;
+                    }
+                    else if (needleIndexCounter > 0)
+                    {
+                        return -1;
+                    }
+                }
+                else {
+                    return resultIndex;
+                }
+            }
+
+            return resultIndex;
+        }
+    }
+}
+/* *****************************************************
+Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+Return 0 when needle is an empty string
+***************************************************** */
