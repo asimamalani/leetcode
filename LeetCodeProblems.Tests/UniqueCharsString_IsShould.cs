a8c7466b67d655d7c0ca774c77155ca22029a360
Asim Amalani <asim.amalani@gmail.com>
2020-05-10 20:06:26 -0400

    Unique characters in a string

---
+++ LeetCodeProblems.Tests/UniqueCharsString_IsShould.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+
+namespace LeetCodeProblems.Tests
+{
+    public class UniqueCharsString_IsShould
+    {
+        private readonly UniqueCharsString _sut;
+
+        public UniqueCharsString_IsShould()
+        {
+            _sut = new UniqueCharsString();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("a")]
+        [InlineData("   a   ")]
+        public void ReturnTrue_ForNullEmptyOneChar(string input)
+        {
+            Assert.True(_sut.Is(input));
+        }
+
+        [Theory]
+        [InlineData("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")]
+        public void ReturnFalse_ForInputExceeds128Chars(string input)
+        {
+            Assert.False(_sut.Is(input));
+        }
+
+        [Theory]
+        [InlineData("this")]
+        public void ReturnTrue_ForUniqueStr(string input)
+        {
+            Assert.True(_sut.Is(input));
+        }
+
+        [Theory]
+        [InlineData("thisis")]
+        public void ReturnFalse_ForNonUniqueCharStr(string input)
+        {
+            Assert.False(_sut.Is(input));
+        }
+    }
+}
---
+++ LeetCodeProblems/UniqueCharsString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCodeProblems
+{
+    public class UniqueCharsString
+    {
+        private const int ASCIICharSetLength = 128;
+
+        public bool Is(string str)
+        {
+
+            // input check for null empty and whitespaces
+            if (string.IsNullOrWhiteSpace(str) || string.Empty == str.Trim())
+            {
+                return true;
+            }
+
+            str = str.Trim();
+
+            // input is unique if only one character
+            if (str.Length == 1)
+            {
+                return true;
+            }
+
+            // assume ASCII input. the charset has 128 chars. input is not unique chars if exceeds the limit
+            if(str.Length > ASCIICharSetLength)
+            {
+                return false;
+            }
+
+            // using an array
+            var map = new bool[ASCIICharSetLength];
+            foreach (var c in str.ToCharArray())
+            {
+                if(map[c])
+                {
+                    return false;
+                }
+                map[c] = true;
+            }
+            return true;
+        }
+    }
+}
