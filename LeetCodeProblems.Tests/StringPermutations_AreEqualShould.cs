a99063870c47ccda57e613d0c25b4ef040b8a3e4
Asim Amalani <asim.amalani@gmail.com>
2020-05-10 22:31:48 -0400

    strings permutations of each other

---
+++ LeetCodeProblems.Tests/StringPermutations_AreEqualShould.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+
+namespace LeetCodeProblems.Tests
+{
+    public class StringPermutations_AreEqualShould
+    {
+        private StringPermutations _sut;
+
+        public StringPermutations_AreEqualShould()
+        {
+            _sut = new StringPermutations();
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("   ", "       ")]
+        public void ReturnTrue_ForNullWhitespace(string str1, string str2)
+        {
+            Assert.True(_sut.AreEqual(str1, str2));
+        }
+
+        [Theory]
+        [InlineData(null, "a")]
+        [InlineData("b", null)]
+        public void ReturnFalse_ForOneNull(string str1, string str2)
+        {
+            Assert.False(_sut.AreEqual(str1, str2));
+        }
+
+        [Theory]
+        [InlineData("helloworld", "helloworld1")]
+        public void ReturnFalse_LengthMismatch(string str1, string str2)
+        {
+            Assert.False(_sut.AreEqual(str1, str2));
+        }
+
+        [Theory]
+        [InlineData("hello", "lhoel")]
+        public void ReturnTrue_ForValidPerm(string str1, string str2)
+        {
+            Assert.True(_sut.AreEqual(str1, str2));
+        }
+
+        [Theory]
+        [InlineData("hellof", "lhoelb")]
+        public void ReturnFalse_ForInvalidPerm(string str1, string str2)
+        {
+            Assert.False(_sut.AreEqual(str1, str2));
+        }
+    }
+}
---
+++ LeetCodeProblems/StringPermutations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCodeProblems
+{
+    public class StringPermutations
+    {
+        public bool AreEqual(string str1, string str2)
+        {
+            // obvious checks
+            if (str1 == str2 || (string.IsNullOrWhiteSpace(str1) && string.IsNullOrWhiteSpace(str2)))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(str1) || string.IsNullOrWhiteSpace(str2))
+            {
+                return false;
+            }
+
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            //// sort method Big O(n log n)
+            //return isPermutationSort(str1, str2);
+
+            // array method
+            return isPermutationArr(str1, str2);
+        }
+
+        private bool isPermutationArr(string str1, string str2)
+        {
+            // assume ASCII char set of 128 length
+
+            // store str1 each char count in an array
+            var charSet = new int[128];
+            for (int i = 0; i < str1.Length; i++)
+            {
+                charSet[str1[i]]++;
+            }
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                charSet[str2[i]]--;
+                if (charSet[str2[i]] < 0) return false;
+            }
+            return true;
+        }
+
+        private bool isPermutationSort(string str1, string str2)
+        {
+            var charArr1 = str1.ToCharArray();
+            Array.Sort(charArr1);
+            //str1 = string.Join("", charArr1);
+            str1 = new string(charArr1);
+
+            var charArr2 = str2.ToCharArray();
+            Array.Sort(charArr2);
+            //str2 = string.Join("", charArr2);
+            str2 = new string(charArr2);
+
+            return str1 == str2;
+        }
+    }
+}
